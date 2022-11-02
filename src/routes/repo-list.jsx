6af22c8ad3f8e9e9a9e8d95a241f25a858af9a44
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-11-02 23:56:55 +0100

    mapped through repo list

--- src/routes/repo-list.jsx
+++ src/routes/repo-list.jsx
@@ -1,23 +1,40 @@
 import { useQuery } from '@tanstack/react-query'
 
 // fetch data from github api using react query
-const fetchUser = async () => {
-  const res = await fetch('https://api.github.com/users/Boye95')
+const fetchRepos = async () => {
+  const res = await fetch('https://api.github.com/users/Boye95/repos')
   return res.json()
 }
 
 export default function RepoList () {
-  const { data, status } = useQuery(['user'], fetchUser)
+  const { data: repo, status } = useQuery(['repos'], fetchRepos)
 
-  //   console.log(data)
+    console.log(repo)
   return (
     <div className='h-[90vh] w-[70%] mx-auto my-auto p-6 rounded-lg shadow-lg bg-white'>
-      <div className=''>
+      <div className='h-full'>
         <h2 className='text-3xl font-urbanist underline decoration-wavy decoration-slate-500'>
           Repositories.
         </h2>
 
-        <div className=""></div>
+        <div className='mt-[3rem] p-2 font-worksans h-[80%] flex flex-col gap-3 overflow-y-scroll'>
+          {status === 'loading' ? (
+            <p>Loading...</p>
+          ) : status === 'error' ? (
+            <p>Error Loading</p>
+          ) : status === 'success' ? (
+            repo.map(repo => {
+              return (
+                <div className='flex justify-between w-[80%] border-2 rounded-lg p-4 shadow-md cursor-pointer hover:ring-2 ring-offset-2'>
+                  <p className=''>{repo.name}</p>
+                  <p className=''>{repo.language}</p>
+                </div>
+              )
+            })
+          ) : (
+            ''
+          )}
+        </div>
       </div>
     </div>
   )
