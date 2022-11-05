4b28ce3897257bc93c111431d050bf1307a0f3be
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-11-05 16:20:56 +0100

    reanamed 404 page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 export { default as Root } from './root'
-export { default as ErrorPage } from './error-page'
+export { default as ErrorPage } from './404'
 export { default as RepoList } from './repo-list'
 export { default as SidebarCard } from './sidebar-card'
 export { default as SingleRepo } from './single-repo'
