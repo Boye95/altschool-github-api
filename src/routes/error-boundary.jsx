8ab95153dd7996b26f96c8b4a912d747efbd4702
Samuel Adeboye Folaranmi <samuel.boye95@yahoo.com>
2022-11-05 16:32:58 +0100

    working on error-boundary

--- src/routes/error-boundary.jsx
+++ src/routes/error-boundary.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 class ErrorBoundary extends React.Component {
   constructor (props) {
@@ -18,17 +17,17 @@ class ErrorBoundary extends React.Component {
   render () {
     if (this.state.hasError) {
       return (
-        <div className='error-boundary'>
-          <p>Something definitely is missing</p>
-          <p>
-            Let's take you back 
-            <Link
-            to='/'
-            className='border-2 border-emerald-100 rounded-lg py-1 px-4 text-white bg-emerald-500 shadow-lg ring-2 ring-emerald-200 ring-offset-2 transition-all hover:text-black hover:bg-white hover:shadow-sm hover:ring-offset-1 sm:text-sm'
-          >
-            Go Back Home
-          </Link>
-          </p>
+        <div className='h-screen w-full flex items-center justify-center font-urbanist'>
+          <div className='flex flex-col items-center justify-center gap-2'>
+            <p className='text-2xl'>Something definitely is missing</p>
+            <p className='text-xl'>Let's take you back</p>
+            <a
+              href='/'
+              className='border-2 border-emerald-100 rounded-lg py-1 px-4 text-white bg-emerald-500 shadow-lg ring-2 ring-emerald-200 ring-offset-2 transition-all hover:text-black hover:bg-white hover:shadow-sm hover:ring-offset-1 sm:text-sm'
+            >
+              Go Back Home
+            </a>
+          </div>
         </div>
       )
     }
